b58762ccd42431101cbb7f48e0c2b5d621fa99f5
Shcoobz <a.boeckmann23@gmail.com>
2024-04-18 16:33:53 +0200

    added functionality to store drawing data

--- script.js
+++ script.js
@@ -49,6 +49,7 @@ brushColorBtn.addEventListener('change', () => {
 bucketColorBtn.addEventListener('change', () => {
   bucketColor = `#${bucketColorBtn.value}`;
   createCanvas();
+  restoreCanvas();
 });
 
 // Eraser
@@ -112,6 +113,9 @@ canvas.addEventListener('mousemove', (event) => {
 
     context.lineTo(currentPosition.x, currentPosition.y);
     context.stroke();
+    storeDrawn(currentPosition.x, currentPosition.y, currentSize, currentColor, isEraser);
+  } else {
+    storeDrawn(undefined);
   }
 });
 
@@ -119,6 +123,48 @@ canvas.addEventListener('mousemove', (event) => {
 canvas.addEventListener('mouseup', () => {
   isMouseDown = false;
 });
+
+// Clear Canvas
+clearCanvasBtn.addEventListener('click', () => {
+  createCanvas();
+  drawnArray = [];
+  // Active Tool
+  activeToolEl.textContent = 'Canvas Cleared';
+  setTimeout(switchToBrush, 1500);
+});
+
+// Draw what is stored in DrawnArray
+function restoreCanvas() {
+  for (let i = 1; i < drawnArray.length; i++) {
+    context.beginPath();
+    context.moveTo(drawnArray[i - 1].x, drawnArray[i - 1].y);
+    context.lineWidth = drawnArray[i].size;
+    context.lineCap = 'round';
+
+    if (drawnArray[i].eraser) {
+      context.strokeStyle = bucketColor;
+    } else {
+      context.strokeStyle = drawnArray[i].color;
+    }
+
+    context.lineTo(drawnArray[i].x, drawnArray[i].y);
+    context.stroke();
+  }
+}
+
+// Store Drawn Lines in DrawnArray
+function storeDrawn(x, y, size, color, erase) {
+  const line = {
+    x,
+    y,
+    size,
+    color,
+    erase,
+  };
+
+  console.log(line);
+  drawnArray.push(line);
+}
 
 // Event Listener
 brushIcon.addEventListener('click', switchToBrush);
